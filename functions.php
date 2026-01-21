7349f67e835e307e60109a171a8c0cd0f9be06ea
Mario Frasca <mario.frasca@comune.trapani.it>
2026-01-21 13:24:55 +0100

    accolti suggerimenti di @enrimk su github.
    rimossa ripetizione caricamento style.css del parent.
    ripescata nel template la handle del caricamento style.css.
    abbassata la priorità di caricamento style.css del child.

--- functions.php
+++ functions.php
@@ -1,20 +1,33 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+/*
+ * metto in coda il caricamento degli stili derivati da
+ * designers-comuni-italia.
 
-function enqueue_child_theme_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
-}
+ * la priorità di caricamento per tutto quello che succede dentro wordpress è
+ * al massimo 10, quindi basta indicare 11 per starci sopra.
 
-//add_action( 'wp_enqueue_scripts', 'mio_tema_child_enqueue_styles', PHP_INT_MAX );
+ * la handle a cui associare questa nostra messa-in-coda la prendo dal
+ * functions.php del tema template, nel corpo della funzione dci_scripts.
 
+ */
+
+add_action( 'wp_enqueue_scripts',
+            function() {
+                wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('dci-wp-style')  );
+            },
+            11);
+
+//add_action( 'wp_enqueue_scripts', 'mio_tema_child_enqueue_styles', 11 );
+
+/*/
 function mio_tema_child_enqueue_styles() {
     // Carica CSS di Bootstrap Italia
     wp_enqueue_style( 'bootstrap-italia-css', get_stylesheet_directory_uri() . '/assets/css/bootstrap-italia-comuni.min.css' );
     // Carica JS di Bootstrap Italia
     wp_enqueue_script( 'bootstrap-italia-js', get_stylesheet_directory_uri() . '/assets/js/bootstrap-italia.min.js', array(), null, true );
 }
+/**/
 
 ?>
 
